187f1ce637452859061b0d38c2fc29a3aa8c6b2e
chicuong2k3 <nguyenchicuongk21@gmail.com>
2025-11-24 17:07:10 +0700

    add manual input

---
+++ src/TowelBorrowing/Components/ReturnDataForManualInput.cs
@@ -0,0 +1,7 @@
+namespace TowelBorrowing.Components;
+
+public class ReturnDataForManualInput
+{
+	public string RoomNo { get; set; } = string.Empty;
+	public string Building { get; set; } = string.Empty;
+}
